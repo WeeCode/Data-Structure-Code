1c631c74c6f90a26bdef270e8df315b0b06e75dc
Pan Chenglong <1004907659@qq.com>
2016-12-29 12:10:27 +0800

    define struct&function of the linked list
    define the node and some common operation function of linked list .

--- code-in-c/DataStuctureInC/DataStuctureInC/ListADT.cpp
+++ code-in-c/DataStuctureInC/DataStuctureInC/ListADT.cpp
@@ -16,3 +16,67 @@
 #include "stdafx.h"
 
 #include "ListADT.h"
+
+struct Node
+{
+	ElementType Element;
+	Position Next;
+};
+
+/*function to operate the linked list ADT */
+List MakeEmpty(List L)
+{
+	return List();
+}
+
+int IsEmpty(List L)
+{
+	return 0;
+}
+
+int IsLast(Position P, List L)
+{
+	return 0;
+}
+
+Position Find(ElementType X, List L)
+{
+	return Position();
+}
+
+void Delete(ElementType X, List L)
+{
+}
+
+Position FindPrevious(ElementType X, List L)
+{
+	return Position();
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+}
+
+void DeleteList(List L)
+{
+}
+
+Position Header(List L)
+{
+	return Position();
+}
+
+Position First(List L)
+{
+	return Position();
+}
+
+Position Advance(Position P)
+{
+	return Position();
+}
+
+ElementType Retrieve(Position P)
+{
+	return ElementType();
+}
--- code-in-c/DataStuctureInC/DataStuctureInC/ListADT.h
+++ code-in-c/DataStuctureInC/DataStuctureInC/ListADT.h
@@ -13,3 +13,31 @@
 * Ëµ    Ã÷£º
 */
 #pragma once
+
+//The Linked List
+#ifndef _ListADT_H
+typedef  int ElementType;
+struct Node;
+typedef	struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+/*operations on the list ADT */
+extern List MakeEmpty(List L);
+extern int IsEmpty(List L);
+extern int IsLast(Position P,List L);
+extern Position Find(ElementType X,List L);
+extern void Delete(ElementType X,List L);
+extern Position FindPrevious(ElementType X,List L);
+extern void Insert(ElementType X,List L,Position P);
+extern void DeleteList(List L);
+extern Position Header(List L);
+extern Position First(List L);
+extern Position Advance(Position P);
+extern ElementType Retrieve(Position P);
+
+#endif
+
+
+
+
