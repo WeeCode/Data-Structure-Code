1e11f2d114e8abc831dd840522f46afa8b11bbd6
Pan Chenglong <1004907659@qq.com>
2016-12-29 11:38:31 +0800

    add file to code the list ADT
    add ListADT.h/ListADT.cpp

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/ListADT.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：ListADT.cpp
+* 文件标识：线性表源文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2016年12月29日
+* 说    明：
+*/
+
+#include "stdafx.h"
+
+#include "ListADT.h"
