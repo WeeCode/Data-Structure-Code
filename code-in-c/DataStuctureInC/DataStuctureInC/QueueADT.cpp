030fc1b8239251a6a6c4fd7308669e61301821d5
Pan Chenglong <1004907659@qq.com>
2017-01-04 22:53:51 +0800

    part of queue operation function
    before test.

--- code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.cpp
+++ code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.cpp
@@ -31,38 +31,83 @@ struct QueueRecord
 
 int IsQueueEmpty(Queue Q)
 {
-	return 0;
+	return Q->Size == 0;
 }
 
 int IsQueueFull(Queue Q)
 {
-	return 0;
+	return Q->Size == Q->Capacity;
 }
 
 Queue CreateQueue(int MaxElements)
 {
-	return Queue();
+	Queue Q;
+
+	Q = (Queue)malloc(sizeof(struct QueueRecord));
+	if (Q == NULL)
+	{
+		perror("CreateQueue error: malloc() out of space!\n");
+		return NULL;
+	}
+
+	Q->Capacity = MaxElements;
+	Q->Size = 0;
+	Q->Front = 1;
+	Q->Rear = 0;
+	Q->Array = (ElementType*)malloc(MaxElements*sizeof(ElementType));
+	if (Q->Array == NULL)
+	{
+		perror("CreateQueue error: malloc() out of space!\n");
+		return NULL;
+	}
+
+	return Q;
 }
 
 void DisposeQueue(Queue Q)
 {
+	free(Q->Array);
+	free(Q);
 }
 
 void MakeQueueEmpty(Queue Q)
 {
+	Q->Size = 0;
+	Q->Front = 1;
+	Q->Rear = 0;
 }
 
 void EnQueue(ElementType X, Queue Q)
 {
+	if (IsQueueFull(Q))
+	{
+		perror("EnQueue error: full queue!\n");
+	}
+	else
+	{
+		Q->Size++;
+		Q->Rear = QueueSucc(Q->Rear,Q);
+		Q->Array[Q->Rear] = X;
+	}
+}
+
+int QueueSucc(int value, Queue Q)
+{
+	if (++value == Q->Capacity)
+	{
+		value = 0;
+	}
+	return value;
 }
 
 ElementType FrontQueue(Queue Q)
 {
-	return ElementType();
+	return Q->Array[Q->Front];
 }
 
 void Dequeue(Queue Q)
 {
+
 }
 
 ElementType FrontAndDequeue(Queue Q)
--- code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.h
+++ code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.h
@@ -27,6 +27,7 @@ extern Queue CreateQueue(int MaxElements);
 extern void DisposeQueue(Queue Q);
 extern void MakeQueueEmpty(Queue Q);
 extern void EnQueue(ElementType X,Queue Q);
+static int QueueSucc(int value,Queue Q);
 extern ElementType FrontQueue(Queue Q);
 extern void Dequeue(Queue Q);
 extern ElementType FrontAndDequeue(Queue Q);
