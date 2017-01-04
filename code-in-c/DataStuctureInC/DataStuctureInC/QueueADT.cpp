49f1a2690aea3874d3067531304615f479d5a779
Pan Chenglong <1004907659@qq.com>
2017-01-04 22:10:21 +0800

    Add queueADT file to project
    Add QueueADT.h/QueueADT.cpp.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：QueueADT.cpp
+* 文件标识：队列-头文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月4日
+* 说    明：由数组实现队列
+*/
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "QueueADT.h"
---
+++ code-in-c/DataStuctureInC/DataStuctureInC/QueueADT.h
@@ -0,0 +1,21 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：QueueADT.h
+* 文件标识：队列-头文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月4日
+* 说    明：由数组实现队列
+*/
+#pragma once
+
+#ifndef _QUEUEADT_H_
+#define _QUEUEADT_H_
+
+#endif // !_QUEUEADT_H_
+
