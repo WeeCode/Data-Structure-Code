c836989f2a30cf19f3c34bedb013210c8b2544dd
Pan Chenglong <1004907659@qq.com>
2017-01-05 17:22:37 +0800

    add Tree.h to project
    Tree.h for common part  of all sorts tree ADT.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/Tree.h
@@ -0,0 +1,21 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：Tree.h
+* 文件标识：各类树结构体通用部分声明头文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月5日
+* 说    明：对各类树结构体的通用部分集中声明
+*/
+#pragma once
+
+#ifndef _TREE_H_
+#define _TREE_H_
+
+#endif
+
