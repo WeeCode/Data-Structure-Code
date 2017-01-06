77161cbc71d4119ee51cd5150b1d8e0589dfb53c
Pan Chenglong <1004907659@qq.com>
2017-01-06 17:56:07 +0800

    Declarations to binary search tree
    Declarations to binary search tree and it's operation function.

--- code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.cpp
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.cpp
@@ -15,4 +15,41 @@
 
 #include "stdafx.h"
 #include <stdlib.h>
+#include "Tree.h"
 #include "BinarySearchTree.h"
+
+
+BinarySearchTree MakeBSTreeEmpty(BinarySearchTree T)
+{
+	return BinarySearchTree();
+}
+
+BStreePos FindBSTree(ElementType X, BinarySearchTree T)
+{
+	return BStreePos();
+}
+
+BStreePos FindMinBSTree(BinarySearchTree T)
+{
+	return BStreePos();
+}
+
+BStreePos FindMaxBSTree(BinarySearchTree T)
+{
+	return BStreePos();
+}
+
+BinarySearchTree InsertBSTree(ElementType X, BinarySearchTree T)
+{
+	return BinarySearchTree();
+}
+
+BinarySearchTree DeleteBSTree(ElementType X, BinarySearchTree T)
+{
+	return BinarySearchTree();
+}
+
+ElementType RetrieveBSTree(BStreePos P)
+{
+	return ElementType();
+}
--- code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.h
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.h
@@ -13,3 +13,20 @@
 * 说    明：二叉查找树
 */
 #pragma once
+
+/*BSTree stand for BinarySearchTree,BStreePos stand for BinarySearchTreePostion*/
+#ifndef _BINARYSEARCHTREE_H_
+#define _BINARYSEARCHTREE_H_
+
+typedef PtrToBinNode BStreePos;
+typedef BinaryTree BinarySearchTree;
+
+BinarySearchTree MakeBSTreeEmpty(BinarySearchTree T);
+BStreePos FindBSTree(ElementType X, BinarySearchTree T);
+BStreePos FindMinBSTree(BinarySearchTree T);
+BStreePos FindMaxBSTree(BinarySearchTree T);
+BinarySearchTree InsertBSTree(ElementType X, BinarySearchTree T);
+BinarySearchTree DeleteBSTree(ElementType X, BinarySearchTree T);
+ElementType RetrieveBSTree(BStreePos P);
+
+#endif // !_BINARYSEARCHTREE_H_
