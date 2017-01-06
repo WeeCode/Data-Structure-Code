0acb7c173a223fb16e99a8141729111ac020d969
Pan Chenglong <1004907659@qq.com>
2017-01-06 17:02:38 +0800

    Add BinarySearchTree ADT operation to project
    add BinarySearchTree.h/BinarySearchTree.cpp.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：BinarySearchTree.cpp
+* 文件标识：二叉查找树-源文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月6日
+* 说    明：二叉查找树
+*/
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "BinarySearchTree.h"
---
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinarySearchTree.h
@@ -0,0 +1,15 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：BinarySearchTree.h
+* 文件标识：二叉查找树-头文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月6日
+* 说    明：二叉查找树
+*/
+#pragma once
