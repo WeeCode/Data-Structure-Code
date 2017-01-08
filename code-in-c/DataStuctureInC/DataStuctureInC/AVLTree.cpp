568b8deb6e7afd1a381901551f9978718bd991da
Pan Chenglong <1004907659@qq.com>
2017-01-08 09:02:32 +0800

    Add AVL tree file to project
    add AVLTree.h/AVLTree.cpp to project.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/AVLTree.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：AVLTree.cpp
+* 文件标识：带平衡条件的二叉查找树-源文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月8日
+* 说    明：带平衡条件的二叉查找树
+*/
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "AVLTree.h"
---
+++ code-in-c/DataStuctureInC/DataStuctureInC/AVLTree.h
@@ -0,0 +1,17 @@
+/*
+* Copyright (c) 2016, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：AVLTree.h
+* 文件标识：带平衡条件的二叉查找树-头文件
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月8日
+* 说    明：带平衡条件的二叉查找树
+*/
+#pragma once
+
+#include "Tree.h"
