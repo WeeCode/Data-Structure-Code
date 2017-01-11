e506b2ccc0854b13b4868423cdfc14f731d3797a
Pan Chenglong <1004907659@qq.com>
2017-01-11 20:45:11 +0800

    Add HashTable ADT  to project
    HashTable implementation of separate chaining, add
    HashSeparateChain.h/.cpp to project.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/HashSeparateChain.h
@@ -0,0 +1,15 @@
+/*
+* Copyright (c) 2017, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：HashSeparateChain.h
+* 文件标识：散列（哈希表）-分离链接法实现
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月11日
+* 说    明：分离链接法实现散列
+*/
+#pragma once
