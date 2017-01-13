b660458737a9fe38c090ca1a52bd65ff532732be
Pan Chenglong <1004907659@qq.com>
2017-01-13 18:49:48 +0800

    Declarations to BinaryHeap
    Declarations to BinaryHeap  and it's operation  function.

--- code-in-c/DataStuctureInC/DataStuctureInC/BinaryHeap.cpp
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinaryHeap.cpp
@@ -3,16 +3,61 @@
 * All rights reserved.
 *
 * 文件名称：BinaryHeap.cpp
-* 文件标识：二叉堆（优先队列）
+* 文件标识：优先队列（二叉堆）
 * 摘    要：
 * 运行平台：windows
 *
 * 版    本：1.00
 * 作    者：潘成龙
 * 日    期：2017年1月13日
-* 说    明：二叉堆（优先队列）- 数组实现
+* 说    明：优先队列（二叉堆）- 数组实现
 */
 
 #include "stdafx.h"
 #include <stdlib.h>
 #include "BinaryHeap.h"
+
+/*implementate binary heap by an array*/
+struct BinaryHeap
+{
+	int Capacity;
+	int Size;
+	ElementType *Elements;
+};
+
+PriorityQueue InitPriQueue(int maxElements)
+{
+	return PriorityQueue();
+}
+
+void DestoryPriQueue(PriorityQueue PriQ)
+{
+}
+
+void MakeEmptyPriQueue(PriorityQueue PriQ)
+{
+}
+
+void InsertPriQueue(ElementType X, PriorityQueue PriQ)
+{
+}
+
+ElementType DeletMinPriQueue(PriorityQueue PriQ)
+{
+	return ElementType();
+}
+
+ElementType FindMinPriQueue(PriorityQueue PriQ)
+{
+	return ElementType();
+}
+
+int IsEmptyPriQueue(PriorityQueue PriQ)
+{
+	return 0;
+}
+
+int IsFullPriQueue(PriorityQueue PriQ)
+{
+	return 0;
+}
--- code-in-c/DataStuctureInC/DataStuctureInC/BinaryHeap.h
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinaryHeap.h
@@ -3,13 +3,38 @@
 * All rights reserved.
 *
 * 文件名称：BinaryHeap.h
-* 文件标识：二叉堆（优先队列）
+* 文件标识：优先队列（二叉堆）
 * 摘    要：
 * 运行平台：windows
 *
 * 版    本：1.00
 * 作    者：潘成龙
 * 日    期：2017年1月13日
-* 说    明：二叉堆（优先队列）- 数组实现
+* 说    明：优先队列（二叉堆）- 数组实现
 */
 #pragma once
+
+#ifndef _BINARYHEAP_H_
+#define _BINARYHEAP_H_
+
+typedef int ElementType;
+typedef struct BinaryHeap *PriorityQueue;
+
+extern PriorityQueue InitPriQueue(int maxElements);
+
+extern void DestoryPriQueue(PriorityQueue PriQ);
+
+extern void MakeEmptyPriQueue(PriorityQueue PriQ);
+
+extern void InsertPriQueue(ElementType X,PriorityQueue PriQ);
+
+extern ElementType DeletMinPriQueue(PriorityQueue PriQ);
+
+extern ElementType FindMinPriQueue(PriorityQueue PriQ);
+
+extern int IsEmptyPriQueue(PriorityQueue PriQ);
+
+extern int IsFullPriQueue(PriorityQueue PriQ);
+
+#endif // !_BINARYHEAP_H_
+
