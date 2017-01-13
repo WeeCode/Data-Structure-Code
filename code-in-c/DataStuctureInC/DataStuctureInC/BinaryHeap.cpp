ec623827a85aabc881d99f2bddc3c3c611710a41
Pan Chenglong <1004907659@qq.com>
2017-01-13 18:34:15 +0800

    Add BinaryHeap ADT to project
    add BinaryHeap.h/.cpp to project

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/BinaryHeap.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2017, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：BinaryHeap.cpp
+* 文件标识：二叉堆（优先队列）
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月13日
+* 说    明：二叉堆（优先队列）- 数组实现
+*/
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "BinaryHeap.h"
