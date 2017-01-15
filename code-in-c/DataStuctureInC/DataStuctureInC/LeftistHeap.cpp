a8ff22f25c103247863a86138b839e0b7dc50e6e
Pan Chenglong <1004907659@qq.com>
2017-01-15 22:37:25 +0800

    Add LeftistHeap ADT to project.
    add LeftistHeap.h/.cpp file.

---
+++ code-in-c/DataStuctureInC/DataStuctureInC/LeftistHeap.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2017, 华中科技大学 国家数控系统工程中心
+* All rights reserved.
+*
+* 文件名称：LeftistHeap.cpp
+* 文件标识：左式堆（优先队列）
+* 摘    要：
+* 运行平台：windows
+*
+* 版    本：1.00
+* 作    者：潘成龙
+* 日    期：2017年1月15日
+* 说    明：左式堆（优先队列）--二叉树实现
+*/
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "LeftistHeap.h"
